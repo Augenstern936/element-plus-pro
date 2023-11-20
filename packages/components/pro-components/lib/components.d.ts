971638b4f32904e6cc7cc83aa0730239a9ff628c
wangbowen936926 <1720581634@qq.com>
2023-11-20 22:57:25 +0800

    抽离公用打包配置

---
+++ packages/components/pro-components/lib/components.d.ts
@@ -0,0 +1,4 @@
+declare const _default: {
+    ProButton: any;
+};
+export default _default;
--- vites/build.base.config.ts
+++ vites/build.base.config.ts
@@ -1,10 +1,9 @@
-import { defineConfig } from "vite";
 import Vue from "@vitejs/plugin-vue";
 import VueJsx from "@vitejs/plugin-vue-jsx";
 import dts from "vite-plugin-dts";
-//import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
 import copy from 'rollup-plugin-copy';
-import { resolve } from "path";
+import path, { resolve } from "path";
 
 const globals = {
     vue: "Vue",
@@ -13,61 +12,74 @@ const globals = {
     "element-plus": "Element-Plus"
 }
 
-const config: Record<string, any> = {
-    build: {
-        target: "es2018",
-        //打包后文件目录
-        outDir: "pro-button",
-        //压缩
-        minify: false,
-        cssCodeSplit: false,
-        terserOptions:{
-            compress:{
-                drop_console: true
+export default function () {
+
+    const cwd = process.cwd();
+
+    const baseDirName = path.basename(cwd);
+
+    const pkgName = `pro-${baseDirName}`;
+
+    const outputRoot = resolve(__dirname, "..", "dist");
+
+    const esOutputDir = `${outputRoot}/${pkgName}/es`;
+
+    const libOutputDir = `${outputRoot}/${pkgName}/lib`;
+
+    const currPkgDir = resolve(outputRoot, pkgName);
+
+    console.log(resolve(cwd, './package.json'))
+    console.log(currPkgDir)
+
+    return {
+        build: {
+            target: "es2018",
+            //打包后文件目录
+            outDir: pkgName,
+            //压缩
+            minify: false,
+            cssCodeSplit: false,
+            lib: {
+                entry: [resolve(cwd, 'src/index.ts')],
+                name: pkgName,
+            },
+            rollupOptions: {
+                //忽略打包vue文件
+                external: ["vue", "@vue/shared", "element-plus", "@element-plus/icons-vue", "@element-plus/pro-utils"],
+                output: [
+                    {
+                        format: "es",
+                        dir: esOutputDir,
+                        entryFileNames: "[name].mjs",
+                        preserveModules: false,
+                        exports: "named",
+                        globals
+                    },{
+                        format: "cjs",
+                        dir: libOutputDir,
+                        entryFileNames: "[name].js",
+                        preserveModules: false,
+                        exports: "named",
+                        globals
+                    }
+                ]
             }
         },
-        lib: {
-            entry: [`./index.ts`],
-            name: 'pro-button',
-        },
-        rollupOptions: {
-            //忽略打包vue文件
-            external: ["vue", "@vue/shared", "element-plus", "@element-plus/icons-vue", "@element-plus/pro-utils"],
-            output: [
-                {
-                    format: "es",
-                    dir: "./pro-button/es",
-                    entryFileNames: "[name].mjs",
-                    preserveModules: false,
-                    exports: "named",
-                    globals
-                },{
-                    format: "cjs",
-                    dir: "./pro-button/lib",
-                    entryFileNames: "[name].js",
-                    preserveModules: false,
-                    exports: "named",
-                    globals
-                }
-            ]
-        }
-    },
-    plugins: [
-        Vue(),
-        VueJsx(),
-        //cssInjectedByJsPlugin(),
-        copy({
-            targets: [
-              { src: './package.json', dest: './pro-button' },
-              { src: './README.md', dest: './pro-button' }, 
-            ]
-        })
-        // dts({
-        //     entryRoot: "./src",
-        //     outDir: ["./pro-button/es", "./pro-button/lib"],
-        //     tsConfigFilePath: "../tsconfig.json"
-        // }),
-    ]
+        plugins: [
+            Vue(),
+            VueJsx(),
+            cssInjectedByJsPlugin(),
+            copy({
+                targets: [
+                  { src: resolve(cwd, './package.json'), dest: currPkgDir },
+                  { src: resolve(cwd, './README.md'), dest: currPkgDir }, 
+                ]
+            }),
+            dts({
+                entryRoot: "./src",
+                outDir: [esOutputDir, libOutputDir],
+                tsConfigFilePath: "../tsconfig.json"
+            })
+        ]
+    }
 }
-
-export default defineConfig(async ({mode}) => config);
