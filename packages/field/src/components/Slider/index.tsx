06761ad07f4a58be12f99f1b1ff362ac52350aa0
wan <17634393156@163.com>
2024-06-05 18:06:56 +0800

    组件完善

--- packages/field/src/components/Slider/index.tsx
+++ packages/field/src/components/Slider/index.tsx
@@ -2,29 +2,33 @@
  * @Description:
  * @Author: wangbowen936926
  * @Date: 2024-03-27 22:42:21
- * @LastEditTime: 2024-06-01 00:41:29
+ * @LastEditTime: 2024-06-05 17:43:52
  * @FilePath: \element-plus-pro\packages\field\src\components\Slider\index.tsx
  */
-import { ElSlider } from 'element-plus';
-import 'element-plus/theme-chalk/src/slider.scss';
-import { FunctionalComponent, defineComponent } from 'vue';
-import { ProFieldSliderProps, proFieldSliderProps } from './props';
-import { useVModel } from '@vueuse/core';
+import { useVModel } from "@vueuse/core";
+import { ElSlider, ElText } from "element-plus";
+import "element-plus/theme-chalk/src/slider.scss";
+import { FunctionalComponent, defineComponent } from "vue";
+import { ProFieldSliderProps, proFieldSliderProps } from "./props";
 
 const ProFieldSlider = defineComponent<ProFieldSliderProps>(
 	(props, ctx) => {
-		const model = useVModel(props, 'modelValue', ctx.emit);
+		const model = useVModel(props, "modelValue", ctx.emit);
 
-		return () => <ElSlider {...props} v-model={model.value} />;
+		return () => (
+			<>
+				{props.mode === "read" ? <ElText>{model.value}</ElText> : <ElSlider {...props} v-model={model.value} />}
+			</>
+		);
 	},
 	{
-		name: 'ProFieldSlider',
+		name: "ProFieldSlider",
 	}
 ) as unknown as FunctionalComponent<ProFieldSliderProps>;
 
 ProFieldSlider.props = proFieldSliderProps as any;
 
-export * from './props';
+export * from "./props";
 
 export { ProFieldSlider };
 
