3f0cc41939011c55a0ca385ca9b3688e1c751514
wan <17634393156@163.com>
2024-07-05 18:09:57 +0800

    添加types

--- packages/types/function.ts
+++ packages/types/function.ts
@@ -2,7 +2,11 @@
  * @Description:
  * @Author: wangbowen936926
  * @Date: 2024-06-29 23:18:22
- * @LastEditTime: 2024-06-29 23:18:28
+ * @LastEditTime: 2024-07-05 13:43:53
  * @FilePath: \element-plus-pro\packages\types\function.ts
  */
-export type ProRequestData<R, P = Record<string, any>> = (params: P, props: any) => Promise<R>;
+export type ProRequestData<R, P = Record<string, any>> = (params: P, props?: any) => Promise<R>;
+
+export type ProRequestOptions = ProRequestData<
+	(string | number | boolean)[] | { label: string; value: string | number | boolean }[]
+>;
