190b64da49c162bc2c5bc36688c681dc8724e853
wan <17634393156@163.com>
2024-07-22 18:07:14 +0800

    pro-table添加搜索栏col-config参数

--- packages/table/src/typing.ts
+++ packages/table/src/typing.ts
@@ -1,8 +1,8 @@
-import type { ProSearchBarProps } from '@element-plus/pro-form';
-import type { ButtonProps } from 'element-plus';
-import type { CSSProperties, ExtractPropTypes, PropType } from 'vue-demi';
-import { JSX } from 'vue/jsx-runtime';
-import { PaginationAlignEnum, StatusColorEnum, ValueTypeEnum } from './enum';
+import type { ProSearchBarProps } from "@element-plus/pro-form";
+import type { ButtonProps, ColProps } from "element-plus";
+import type { CSSProperties, ExtractPropTypes, PropType } from "vue-demi";
+import { JSX } from "vue/jsx-runtime";
+import { PaginationAlignEnum, StatusColorEnum, ValueTypeEnum } from "./enum";
 
 export const proTableProps = {
 	loading: {
@@ -23,7 +23,7 @@ export const proTableProps = {
 	},
 	cellAlign: {
 		type: String as PropType<GlobalCellAlign>,
-		default: 'left',
+		default: "left",
 	},
 	headerCellStyle: {
 		type: Object as PropType<CSSProperties>,
@@ -37,7 +37,7 @@ export const proTableProps = {
 	// 为空时，默认显示的标记
 	columnEmptyText: {
 		type: [Boolean, String],
-		default: '-',
+		default: "-",
 	},
 	// 默认分页大小
 	defaultSize: {
@@ -58,7 +58,7 @@ export const proTableProps = {
 	toolbar: {
 		type: Object as PropType<TableToolbarConfig[]>,
 		default: {
-			placeholder: '请输入关键字',
+			placeholder: "请输入关键字",
 			showAction: true,
 			actionStyle: {},
 		},
@@ -115,15 +115,15 @@ export type TableColumns = {
 	dataField?: string; // 列字段名称(与数据源中字段对应进行值的映射)
 	//type?: ColumnType; // selection：多选 index：序号 expand：可展开的按钮
 	width?: string | number; // 列宽
-	fixed?: boolean | 'left' | 'right'; // 固定列
-	align?: 'left' | 'right' | 'center';
+	fixed?: boolean | "left" | "right"; // 固定列
+	align?: "left" | "right" | "center";
 	tooltip?: string | ((row: { [x: string]: any }) => string); // 提示文本
 	ellipsis?: boolean; // 是否显示省略号
 	hideInTable?: boolean; // 是否在表格中不显示该列
 	sorter?: boolean; // 排序
 	copyable?: boolean; //是否支持复制
 	filters?: boolean | ValueOption; // 是否在表格列标头开启筛选
-	search?: boolean | ColumnSearchConfig; // 是否在搜索栏中显示该项
+	search?: boolean | ProTableColumnSearchConfig; // 是否在搜索栏中显示该项
 	valueType?: ValueType; // 当前筛选项输入框类型
 	//valueOption?: ValueOption; // 筛选项数据集合
 	valueEnum?: ValueEnum;
@@ -142,26 +142,30 @@ export type ValueOption = {
 
 export type ValueEnum = Record<string, string | number | { text: string; status?: keyof typeof StatusColorEnum }>;
 
-export type GlobalCellAlign = 'left' | 'right' | 'center';
+export type GlobalCellAlign = "left" | "right" | "center";
 
 /**
  * 全局筛选栏配置
  */
-export type GlobalSearchConfig = Omit<ProSearchBarProps, 'modelValue' | 'columns'> & {
-	colSpan?: number;
-	// maxShow?: number;
-	// inline?: boolean;
-	// labelWidth?: number | string;
-	// rightTools?: ToolbarActions;
+export type GlobalSearchConfig = Omit<ProSearchBarProps, "modelValue" | "columns"> & {
+	colSpan?: number | ProTableSearchColConfig;
+};
+
+export type ProTableSearchColConfig = {
+	xs?: number | Pick<ColProps, "span" | "offset" | "pull" | "push">;
+	sm?: number | Pick<ColProps, "span" | "offset" | "pull" | "push">;
+	md?: number | Pick<ColProps, "span" | "offset" | "pull" | "push">;
+	lg?: number | Pick<ColProps, "span" | "offset" | "pull" | "push">;
+	xl?: number | Pick<ColProps, "span" | "offset" | "pull" | "push">;
 };
 
 /**
  * 筛选栏配置
  */
-export type ColumnSearchConfig = {
+export type ProTableColumnSearchConfig = {
 	order?: number;
 	label?: string;
-	labelWidth?: 'auto' | number | string;
+	labelWidth?: "auto" | number | string;
 	placeholder?: string;
 	fieldProps?: {
 		options?: { label: string; value: string | number | boolean }[];
